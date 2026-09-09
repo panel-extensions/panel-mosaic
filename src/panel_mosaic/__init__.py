62000816bfd3b5629bb5f446618e3f0f223b9554
Sumaiya Islam <bsse1446@iit.du.ac.bd>
2026-09-09 14:59:09 +0600

    Use an available PyPI distribution name (#3)

--- src/panel_mosaic/__init__.py
+++ src/panel_mosaic/__init__.py
@@ -6,7 +6,7 @@ import warnings
 from panel_mosaic.mosaic import Mosaic
 
 try:
-    __version__ = importlib.metadata.version(__name__)
+    __version__ = importlib.metadata.version("panel-mosaic-viz")
 except importlib.metadata.PackageNotFoundError as e:  # pragma: no cover
     warnings.warn(f"Could not determine version of {__name__}\n{e!s}", stacklevel=2)
     __version__ = "unknown"
